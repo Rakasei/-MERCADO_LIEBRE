0ff25ed747885997462d1eeb9eafcae198e69978
Rodrigo Ezequiel Ruiz <98621578+Rakasei@users.noreply.github.com>
2024-07-29 11:29:55 -0300

    Cambios agregados

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ app.get('/', (req, res)=>{
 app.get('/home', (req, res)=>{
     res.redirect('/')
 })
+app.get("/register", (req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+});
+app.get("/login", (req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+});
 
 
 
